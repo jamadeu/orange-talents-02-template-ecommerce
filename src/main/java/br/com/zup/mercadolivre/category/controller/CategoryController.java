dc2362583b9a44b10d94aa0e4a016c22986a4060
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-16 10:25:36 -0300

    feat: Find Category By Id

--- src/main/java/br/com/zup/mercadolivre/category/controller/CategoryController.java
+++ src/main/java/br/com/zup/mercadolivre/category/controller/CategoryController.java
@@ -4,15 +4,13 @@ import br.com.zup.mercadolivre.category.dto.NewCategoryRequest;
 import br.com.zup.mercadolivre.category.model.Category;
 import br.com.zup.mercadolivre.category.repository.CategoryRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
@@ -22,6 +20,15 @@ public class CategoryController {
 
     public CategoryController(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optionalCategory.get());
     }
 
     @PostMapping
