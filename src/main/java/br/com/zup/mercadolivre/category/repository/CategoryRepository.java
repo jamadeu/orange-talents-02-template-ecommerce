46b55ee5077c9ed3a58a15668ffd5ce8dfedaa84
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-25 08:12:09 -0300

    refact category tests

--- src/main/java/br/com/zup/mercadolivre/category/repository/CategoryRepository.java
+++ src/main/java/br/com/zup/mercadolivre/category/repository/CategoryRepository.java
@@ -4,6 +4,9 @@ import br.com.zup.mercadolivre.category.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
--- src/test/java/br/com/zup/mercadolivre/category/controller/CategoryControllerTest.java
+++ src/test/java/br/com/zup/mercadolivre/category/controller/CategoryControllerTest.java
@@ -3,54 +3,137 @@ package br.com.zup.mercadolivre.category.controller;
 import br.com.zup.mercadolivre.category.dto.NewCategoryRequest;
 import br.com.zup.mercadolivre.category.model.Category;
 import br.com.zup.mercadolivre.category.repository.CategoryRepository;
-import org.assertj.core.api.Assertions;
+import br.com.zup.mercadolivre.user.controller.UserCreator;
+import br.com.zup.mercadolivre.user.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@ExtendWith(SpringExtension.class)
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
 class CategoryControllerTest {
 
-    @InjectMocks
-    private CategoryController categoryController;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @Mock
+    @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Long idMotherCategory;
 
     @BeforeEach
     void setup() {
-        BDDMockito.when(categoryRepository.save(ArgumentMatchers.any(Category.class)))
-                .thenReturn(CategoryCreator.createCategory());
+        userRepository.save(UserCreator.createUserPasswordEncrypted());
+        Category motherCategory = categoryRepository.save(CategoryCreator.createMotherCategory());
+        this.idMotherCategory = motherCategory.getId();
     }
 
     @Test
+    @WithMockUser(username = "user@email.com", password = "123456")
     @DisplayName("create returns status code 200 when successful")
-    void test1() {
-        NewCategoryRequest newCategoryRequest = CategoryCreator.createNewCategoryRequestIdMotherNull();
-        ResponseEntity<?> response = categoryController.create(newCategoryRequest);
+    void test1() throws Exception {
+        NewCategoryRequest newCategoryRequest = CategoryCreator.createNewCategoryRequest("Category", idMotherCategory);
 
-        Assertions.assertThat(response).isNotNull();
-        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/category")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newCategoryRequest))
+        ).andExpect(MockMvcResultMatchers
+                .status()
+                .isOk()
+        ).andReturn();
+
+        Category category = categoryRepository.findByName(newCategoryRequest.getName()).orElseThrow();
+
+        assertEquals(category.getName(), newCategoryRequest.getName());
     }
 
     @Test
-    @DisplayName("create returns status code 400 when category mother is not found")
-    void test2() {
-        NewCategoryRequest newCategoryRequest = CategoryCreator.createNewCategoryRequest(
-                "Category",
-                1L
-        );
-        ResponseEntity<?> response = categoryController.create(newCategoryRequest);
+    @WithMockUser(username = "user@email.com", password = "123456")
+    @DisplayName("create returns status code 200 when idMotherCategory is null")
+    void test2() throws Exception {
+        NewCategoryRequest newCategoryRequest = CategoryCreator.createNewCategoryRequestIdMotherNull();
 
-        Assertions.assertThat(response).isNotNull();
-        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/category")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newCategoryRequest))
+        ).andExpect(MockMvcResultMatchers
+                .status()
+                .isOk()
+        );
+
+        Category category = categoryRepository.findByName(newCategoryRequest.getName()).orElseThrow();
+
+        assertEquals(category.getName(), newCategoryRequest.getName());
+    }
+
+    @Test
+    @WithMockUser(username = "user@email.com", password = "123456")
+    @DisplayName("create returns status code 400 when idMotherCategory is invalid")
+    void test3() throws Exception {
+        NewCategoryRequest newCategoryRequest = CategoryCreator.createNewCategoryRequest("Category", idMotherCategory + 1);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/category")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newCategoryRequest))
+        ).andExpect(MockMvcResultMatchers
+                .status()
+                .isBadRequest()
+        ).andExpect(MockMvcResultMatchers
+                .content()
+                .string("Mother Category not found")
+        );
+
+        Optional<Category> optionalCategory = categoryRepository.findByName(newCategoryRequest.getName());
+
+        assertTrue(optionalCategory.isEmpty());
+    }
+
+    @Test
+    @WithMockUser(username = "user@email.com", password = "123456")
+    @DisplayName("create returns status code 400 when name is invalid")
+    void test4() throws Exception {
+        NewCategoryRequest newCategoryRequest = CategoryCreator.createNewCategoryRequest(null, idMotherCategory + 1);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/category")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newCategoryRequest))
+        ).andExpect(MockMvcResultMatchers
+                .status()
+                .isBadRequest()
+        );
+
+        Optional<Category> optionalCategory = categoryRepository.findByName(newCategoryRequest.getName());
+
+        assertTrue(optionalCategory.isEmpty());
     }
 }
